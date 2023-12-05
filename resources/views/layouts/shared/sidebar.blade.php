fe68dace47033c133114b90dcb719555100b5d9a
IronMan304 <jerechoreyinatilleza@gmail.com>
2023-12-05 15:52:30 +0800

    swith to blade stack

---
+++ resources/views/layouts/shared/sidebar.blade.php
@@ -0,0 +1,105 @@
+{{-- @if (auth()->user()->hasrole(['doctor', 'staff', 'admin', 'Inquiry'])) --}}
+@if (!auth()->user()->hasRole('Patient'))
+	<div class="sidebar" id="sidebar">
+		<div class="sidebar-inner slimscroll">
+			<div class="sidebar-menu" id="sidebar-menu">
+				<ul>
+					<li>
+						<a href="/"><span class="menu-side"><i class="fa-solid fa-house"></i></span>
+							<span>Dashboard</span></a>
+					</li>
+					@hasrole(['admin', 'staff'])
+						<li class="submenu">
+							<a href="#"><span class="menu-side"><i class="fa-solid fa-user-group"></i></span>
+								<span>Users</span> <span class="menu-arrow"></span>
+							</a>
+
+							<ul style="display: none;">
+								@hasrole('admin')
+									<li><a href="/user">User Management</a></li>
+									<li><a href="/doctor">Doctors</a></li>
+									<li><a href="/staff">Staff</a></li>
+								@endhasrole
+								<li><a href="/patient">Patients</a></li>
+							</ul>
+						</li>
+					@endhasrole
+					@hasrole(['doctor', 'staff', 'admin'])
+						<li>
+							<a href="/appointment"><span class="menu-side"><i class="fa-solid fa-calendar-day"></i></span>
+								<span>Appointments</span>
+							</a>
+						</li>
+						<li>
+							<a href="/booking"><span class="menu-side"><i class="fa-solid fa-calendar-check"></i></span>
+								<span>Booking</span></a>
+						</li>
+						@hasrole(['staff', 'admin'])
+							<li>
+								<a href="/billing"><span class="menu-side"><i class="fa-solid fa-wallet"></i></span>
+									<span>Billing</span></a>
+							</li>
+						@endhasrole
+					@endhasrole
+					@hasrole(['supervisor', 'encoder', 'admin'])
+						<li>
+							<a href="/ekonsulta"><span class="menu-side"><i class="fa-solid fa-stethoscope"></i></span>
+								<span>Ekonsulta</span>
+							</a>
+						</li>
+					@endhasrole
+					@hasrole(['supervisor', 'admin', 'inquiry'])
+						<li class="submenu">
+							<a href="#"><span class="menu-side"><i class="fa-solid fa-list"></i></span>
+								<span>Reports</span> <span class="menu-arrow"></span>
+							</a>
+
+							<ul style="display: none;">
+								@hasrole(['supervisor', 'admin'])
+									<li><a href="/report">Employee Efficiency Report</a></li>
+									<li><a href="/billingreport">Front Office Report</a></li>
+									<li><a href="/booking_report1">Booking Report </a></li>
+								@endhasrole
+								<li><a href="/booking_specific_report">Company Report</a></li>
+								<li><a href="/company_physical_exam">PE Report</a></li>
+								@hasrole(['supervisor', 'admin'])
+									<li><a href="/examinationreport">Examination Report</a></li>
+								@endhasrole
+							</ul>
+						</li>
+					@endhasrole
+					@hasrole(['admin', 'staff'])
+						<li class="menu-title">Setup</li>
+
+						<li class="submenu">
+							<a href="#"><i class="fa fa-gear"></i> <span>System</span> <span class="menu-arrow"></span></a>
+							<ul style="display: none;">
+								@hasrole('admin')
+									<li><a href="/status">Status</a></li>
+									<li><a href="/specialization">Specialization</a></li>
+									<li><a href="/department">Departments</a></li>
+									<li><a href="/branch">Branch</a></li>
+								@endhasrole
+								<li><a href="/services">Services</a></li>
+								<li><a href="/company">Companies</a></li>
+								<li><a href="/discount">Discount</a></li>
+								<li><a href="/units">Unit</a></li>
+								<li><a href="/actives">Actives</a></li>
+							</ul>
+						</li>
+						@hasrole('admin')
+							<li class="submenu mb-5">
+								<a href="#"><i class="fa fa-user-shield"></i> <span>Authentication</span> <span
+										class="menu-arrow"></span></a>
+								<ul style="display: none;">
+									<li><a href="/role">Role</a></li>
+									<li class="mb-5"><a href="/permission">Permission</a></li>
+								</ul>
+							</li>
+						@endhasrole
+					@endhasrole
+				</ul>
+			</div>
+		</div>
+	</div>
+@endif
