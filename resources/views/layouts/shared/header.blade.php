2ed3a44d065241e4498b7ba5637ea994a8578df2
IronMan304 <jerechoreyinatilleza@gmail.com>
2024-05-07 18:39:54 +0800

    small changes

--- resources/views/layouts/shared/header.blade.php
+++ resources/views/layouts/shared/header.blade.php
@@ -143,7 +143,7 @@
 		</div>
 	</div>
 </div>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="{{ asset('assets/js/jquery-3.6.0.min.js') }}"></script>
 <script>
 	$(document).ready(function() {
 		function updateRealtimeCount() {
